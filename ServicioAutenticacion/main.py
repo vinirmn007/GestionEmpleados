f677fc9a9b954ac8c9b2ea1ee38534620d20b276
Alexis Roman <alexis.roman@unl.edu.ec>
2025-10-21 10:35:10 -0500

    endpoints and dockerfile

--- ServicioAutenticacion/main.py
+++ ServicioAutenticacion/main.py
@@ -5,3 +5,15 @@ app = FastAPI(title="Servicio Autenticacion", version="1.0.0")
 @app.get("/")
 def read_root():
     return {"mensaje": "¡Hola, Servicio Autenticacion está funcionando!"}
+
+@app.post("auth/login")
+def login(email: str, password: str):
+    return {"mensaje": f"Intento de inicio de sesión para {email}"}
+
+@app.post("auth/logout")
+def logout():
+    return {"mensaje": "¡Adiós desde el Servicio Autenticacion!"}
+
+@app.post("auth/refresh")
+def refresh_token():
+    return {"mensaje": "Token de autenticación renovado"}
