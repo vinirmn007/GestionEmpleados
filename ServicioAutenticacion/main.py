6e52263860661f75f72e05b33fc083ef1e826f8b
Alexis Roman <alexis.roman@unl.edu.ec>
2025-10-26 21:50:24 -0500

    implementacion de cors

--- ServicioAutenticacion/main.py
+++ ServicioAutenticacion/main.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Request
 from datetime import timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi_jwt_auth3 import AuthJWT
-#from fastapi_jwt_auth3.exceptions import AuthJWTException
-from pydantic import BaseModel
-from fastapi import Request
 from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
 
-from .models import models, db_model
-from .secret_keys_settings import settings
+from models.models import *
+from secret_keys_settings import settings
 
 app = FastAPI(title="Servicio Autenticacion", version="1.0.0")
 
@@ -16,21 +15,41 @@ app = FastAPI(title="Servicio Autenticacion", version="1.0.0")
 def read_root():
     return {"mensaje": "¡Hola, Servicio Autenticacion está funcionando!"}
 
+#carga config del .env
 @AuthJWT.load_config
 def get_config():
     return settings
 
-@app.post("auth/login", response_model=models.Token, tags=["Autenticacion"])
-def login(data: models.LoginRequest, Authorize: AuthJWT = Depends()):
+
+#origenes permitidos
+origins = [
+    #"http://localhost:8080",
+    "*"
+]
+
+#cinfigurar cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/auth/login", response_model=Token, tags=["Autenticacion"])
+def login(data: LoginRequest, Authorize: AuthJWT = Depends()):
     #LO SIGUIENTE SE OBTIENE CON EL SERVICIO DE USUARIOS
     #is_valid_user = validar que exista ese user
     #user_id = obtener el user id del usuario que hace la peticion
     #user_roles = obtener rles de usuario de la bd
 
     #SIMULACIONNNNNNNNNN:
-    is_valid_user = True
+    is_valid_user = False
     user_id = 1
     user_roles = ["user", "admin"]
+
+    x = data.email
+    print(x)
 
     if not is_valid_user:
         raise HTTPException(
@@ -47,13 +66,13 @@ def login(data: models.LoginRequest, Authorize: AuthJWT = Depends()):
         user_claims=claims,
         expires_time=timedelta(hours=1)
     )
+
         
     return {
         "access_token": access_token,
         "token_type": "bearer"
     }
-    pass
 
-@app.post("auth/logout")
+@app.post("/auth/logout")
 def logout():
     return {"mensaje": "¡Adiós desde el Servicio Autenticacion!"}
