06b25418585dd128b842e60da6ecbaa6f72f721d
Alexis Roman <alexis.roman@unl.edu.ec>
2025-10-27 22:35:47 -0500

    fix errors

--- ServicioAutenticacion/main.py
+++ ServicioAutenticacion/main.py
@@ -12,7 +12,7 @@ from secret_keys_settings import settings
 
 app = FastAPI(title="Servicio Autenticacion", version="1.0.0")
 
-@app.get("/")
+@app.get("/auth")
 def read_root():
     return {"mensaje": "¡Hola, Servicio Autenticacion está funcionando!"}
 
