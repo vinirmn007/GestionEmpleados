1925f2282c704bdf201ffc7467dc660acd504cb2
Alexis Roman <alexis.roman@unl.edu.ec>
2025-11-06 09:05:42 -0500

    update autenticacion

--- ServicioAutenticacion/main.py
+++ ServicioAutenticacion/main.py
@@ -1,47 +1,32 @@
-from fastapi import FastAPI, Depends, HTTPException, status, Request
-from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import httpx
 
-from fastapi.responses import JSONResponse
-from fastapi_jwt_auth import AuthJWT
-from fastapi_jwt_auth.exceptions import AuthJWTException
-
 from models.models import *
-from secret_keys_settings import settings
+from controller import create_access_token
 
 app = FastAPI(title="Servicio Autenticacion", version="1.0.0")
 
-@app.get("/auth")
-def read_root():
-    return {"mensaje": "¡Hola, Servicio Autenticacion está funcionando!"}
-
-#carga config del .env
-@AuthJWT.load_config
-def get_config():
-    return settings
-
-
-#origenes permitidos
-origins = [
-    #"http://localhost:8080",
-    "*"
-]
-
-#cinfigurar cors
+# --- Configurar CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # ajusta según necesidad
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-#Para pruebas: USER_SERVICE_URL = "http://localhost:8000"
-USER_SERVICE_URL = "http://servicio-usuarios:9001"
+#USER_SERVICE_URL = "http://servicio-usuarios:9001"
+USER_SERVICE_URL = "http://127.0.0.1:9001"
+
+@app.get("/auth")
+def read_root():
+    return {"mensaje": "¡Hola, Servicio Autenticacion está funcionando!"}
+
 
 @app.post("/auth/login", response_model=Token, tags=["Autenticacion"])
-async def login(data: LoginRequest, Authorize: AuthJWT = Depends()):
+async def login(data: LoginRequest):
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post(
@@ -51,28 +36,23 @@ async def login(data: LoginRequest, Authorize: AuthJWT = Depends()):
 
             if response.status_code in (401, 404):
                 raise HTTPException(status_code=401, detail="Correo o contraseña incorrectos")
-                        
             response.raise_for_status()
+
         except httpx.RequestError as exc:
-            raise HTTPException(status_code=503, detail=f"Servicio de usuarios invalido: {exc}")
+            raise HTTPException(status_code=503, detail=f"Servicio de usuarios inválido: {exc}")
         
     user_data = response.json()
-    
-    claims = {
-        "roles": user_data["rol"]
-    }
-    
-    access_token = Authorize.create_access_token(
-        subject=user_data["id"],
-        user_claims=claims,
-        expires_time=timedelta(hours=1)
-    )
 
-        
+    access_token = create_access_token({
+        "sub": user_data["id"],
+        "roles": user_data["rol"]
+    })
+
     return {
         "access_token": access_token,
         "token_type": "bearer"
     }
+
 
 @app.post("/auth/logout")
 def logout():
